e3aa055610ae39e14573af9514244fcca07a3bf4
GauravP1407 <gauravpatil1407@gmail.com>
2023-04-12 17:18:53 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/room_locator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.room_locator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:room_locator/add_page.dart';
+import 'package:room_locator/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currpage = 0;
+
+  List<Widget> pages = [const HomePage(), const AddPage()];
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        key: _scaffoldKey,
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(60),
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.purple.shade900,
+                  Colors.purple.shade800,
+                  Colors.purple.shade700,
+                  Colors.purple.shade600,
+                ],
+              ),
+            ),
+            child: SafeArea(
+                child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: AnimatedOpacity(
+                    opacity: currpage == 0 ? 1.0 : 0.0,
+                    duration: const Duration(milliseconds: 500),
+                    child: const Text(
+                      "Home",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                    alignment: Alignment.center,
+                    child: AnimatedOpacity(
+                      opacity: currpage == 1 ? 1.0 : 0.0,
+                      duration: const Duration(milliseconds: 500),
+                      child: const Text(
+                        'Add Page',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: IconButton(
+                    icon: const Icon(Icons.menu),
+                    onPressed: () {
+                      _scaffoldKey.currentState!.openDrawer();
+                    },
+                  ),
+                ),
+              ],
+            )),
+          ),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const UserAccountsDrawerHeader(
+                accountName: Text('Promod Adde'),
+                accountEmail: Text('PramodAdde@gmail.com'),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: AssetImage('images/profile.jpg'),
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.search),
+                title: const Text('Search Property'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.add),
+                title: const Text('Post Free Ad'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.payment),
+                title: const Text('Pay Your Rent'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.person),
+                title: const Text('About Developer'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+        body: pages[currpage],
+        bottomNavigationBar: NavigationBar(
+          destinations: const [
+            NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+            NavigationDestination(icon: Icon(Icons.business), label: "Ads"),
+          ],
+          onDestinationSelected: (int index) {
+            setState(() {
+              currpage = index;
+            });
+          },
+          selectedIndex: currpage,
+        ),
+      ),
+    );
+  }
+}
